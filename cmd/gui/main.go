89b4c5732b0eca3987b0135f5df702be1484c972
Aleksey Timchenko <flegmatik.bel@gmail.com>
2024-03-01 16:43:14 +0300

    feat: basic primitives and triangles

---
+++ cmd/gui/main.go
@@ -0,0 +1,163 @@
+package main
+
+import (
+	"gogl/gogl"
+	"math"
+	"os"
+
+	"github.com/veandco/go-sdl2/sdl"
+)
+
+const ratio int = 100
+const width int = 16 * ratio
+const height int = 9 * ratio
+
+const red = 0xFF2020FF
+const green = 0xFF20FF20
+const blue = 0xFFFF2020
+const gray = 0xFF202020
+
+// TODO float32 math?
+var dx float64 = 1
+var dy float64 = 1
+var step float64 = 5
+
+var ad float64 = 1.0
+
+var x1 float64 = float64(width / 2)
+var y1 float64 = float64(height / 2)
+var x2 float64 = float64(width/2 + width/4)
+var y2 float64 = float64(0)
+var x3 float64 = float64(width)
+var y3 float64 = float64(height / 4)
+
+func moveX(px float64) float64 {
+	x := px + dx*step
+	if x < 0 || x > float64(width) {
+		dx *= -1
+		return px
+	}
+	return x
+}
+
+func moveY(py float64) float64 {
+	y := py + dy*step
+	if y < 0 || y > float64(height) {
+		dy *= -1
+		return py
+	}
+	return y
+}
+
+func movePoint(px float64, py float64) (float64, float64) {
+	return moveX(px), moveY(py)
+}
+
+func triangleCenter(x1 float64, y1 float64, x2 float64, y2 float64, x3 float64, y3 float64) (float64, float64) {
+	return (x1 + x2 + x3) / 3.0, (y1 + y2 + y3) / 3.0
+}
+
+func rotatePoint(x float64, y float64, angle float64, cx float64, cy float64) (float64, float64) {
+	dx := x - cx
+	dy := y - cy
+
+	cos := math.Cos(angle)
+	sin := math.Sin(angle)
+	xf := dx*cos - dy*sin + cx
+	yf := dx*sin + dy*cos + cy
+
+	if x < 0 || x > float64(width) {
+		ad *= -1.0
+		xf = x
+	}
+	if yf < 0 || yf > float64(height) {
+		ad *= -1.0
+		yf = y
+	}
+
+	return xf, yf
+}
+
+func render(c *gogl.Canvas, dt float64) {
+	angle := 0.1 * ad * dt * math.Pi
+
+	cx, cy := triangleCenter(x1, y1, x2, y2, x3, y3)
+	x1, y1 = movePoint(x1, y1)
+	x2, y2 = movePoint(x2, y2)
+	x3, y3 = movePoint(x3, y3)
+
+	x1, y1 = rotatePoint(x1, y1, angle, cx, cy)
+	x2, y2 = rotatePoint(x2, y2, angle, cx, cy)
+	x3, y3 = rotatePoint(x3, y3, angle, cx, cy)
+
+	c.Fill(gray)
+
+	c.FillRGBTriangle(int(x1), int(y1), int(x2), int(y2), int(x3), int(y3))
+}
+
+// TODO move to function to support multiple examples
+func main() {
+	if err := sdl.Init(sdl.INIT_VIDEO); err != nil {
+		panic(err)
+	}
+	defer sdl.Quit()
+
+	window, err := sdl.CreateWindow(
+		"gogl",
+		sdl.WINDOWPOS_CENTERED,
+		sdl.WINDOWPOS_CENTERED,
+		int32(width),
+		int32(height),
+		sdl.WINDOW_SHOWN,
+	)
+	if err != nil {
+		panic(err)
+	}
+	defer window.Destroy()
+
+	renderer, err := sdl.CreateRenderer(window, -1, sdl.RENDERER_ACCELERATED|sdl.RENDERER_PRESENTVSYNC)
+	if err != nil {
+		panic(err)
+	}
+	defer renderer.Destroy()
+
+	canvas := gogl.NewCanvas(width, height)
+
+	prevTicks := sdl.GetTicks()
+	for {
+		ticks := sdl.GetTicks()
+		dt := float64(ticks-prevTicks) / 1000.0
+		prevTicks = ticks
+
+		for event := sdl.PollEvent(); event != nil; event = sdl.PollEvent() {
+			switch event.(type) {
+			case *sdl.QuitEvent:
+				os.Exit(0)
+				break
+			}
+		}
+
+		render(canvas, dt)
+
+		texture, err := renderer.CreateTexture(uint32(sdl.PIXELFORMAT_RGBA32), sdl.TEXTUREACCESS_STREAMING, int32(width), int32(height))
+		if err != nil {
+			panic(err)
+		}
+
+		pixels, _, err := texture.Lock(nil)
+		copy(pixels, canvas.AsBytes())
+		texture.Unlock()
+
+		err = renderer.Copy(texture, nil, nil)
+		if err != nil {
+			panic(err)
+		}
+
+		renderer.Present()
+
+		err = texture.Destroy()
+		if err != nil {
+			panic(err)
+		}
+	}
+}
---
+++ gogl/canvas.go
@@ -0,0 +1,205 @@
+package gogl
+
+import "unsafe"
+
+type (
+	Canvas struct {
+		Width  int
+		Height int
+
+		Pixels []uint32
+	}
+)
+
+func (canvas *Canvas) GetPixel(x int, y int) uint32 {
+	if x >= canvas.Width || y >= canvas.Height || x < 0 || y < 0 {
+		return 0
+	}
+
+	return canvas.Pixels[y*canvas.Width+x]
+}
+
+func (canvas *Canvas) PutPixel(x int, y int, color uint32) {
+	if x >= canvas.Width || y >= canvas.Height || x < 0 || y < 0 {
+		return
+	}
+
+	canvas.Pixels[y*canvas.Width+x] = color
+}
+
+func (canvas *Canvas) AsBytes() []byte {
+	l := len(canvas.Pixels)
+
+	if l == 0 {
+		return nil
+	}
+
+	size := unsafe.Sizeof(canvas.Pixels[0])
+
+	return unsafe.Slice((*byte)(unsafe.Pointer(&canvas.Pixels[0])), int(size)*l)
+}
+
+func (canvas *Canvas) Fill(color uint32) {
+	for y := 0; y < canvas.Height; y++ {
+		for x := 0; x < canvas.Width; x++ {
+			canvas.PutPixel(x, y, color)
+		}
+	}
+}
+
+// TODO: improve it, this is not optimized code, first thing that came to my mind
+func (canvas *Canvas) Line(x1 int, y1 int, x2 int, y2 int, color uint32) {
+	if x1 > x2 {
+		x1, x2 = x2, x1
+		y1, y2 = y2, y1
+	}
+
+	dx := x2 - x1
+	dy := y2 - y1
+
+	var ydir int
+	if y2 > y1 {
+		ydir = 1
+	} else {
+		ydir = -1
+	}
+
+	if dx == 0 {
+		for y := y1; y != y2+ydir; y += ydir {
+			canvas.PutPixel(x1, y, color)
+		}
+		return
+	}
+
+	yp := y1
+	for x := x1; x <= x2; x++ {
+		yn := dy*x/dx - dy*x1/dx + y1
+		for y := yp; y != yn+ydir; y += ydir {
+			canvas.PutPixel(x, y, color)
+		}
+		yp = yn
+	}
+}
+
+func (canvas *Canvas) FillRect(left int, bottom int, w int, h int, color uint32) {
+	for y := bottom; y <= bottom+h; y++ {
+		for x := left; x <= left+w; x++ {
+			canvas.PutPixel(x, y, color)
+		}
+	}
+}
+
+func (canvas *Canvas) FillCircle(cx int, cy int, r int, color uint32) {
+	for y := -r; y <= r; y++ {
+		for x := -r; x <= r; x++ {
+			if x*x+y*y <= r*r {
+				canvas.PutPixel(x+cx, y+cy, color)
+			}
+		}
+	}
+}
+
+func min(a int, b int) int {
+	if a < b {
+		return a
+	}
+	return b
+}
+
+func max(a int, b int) int {
+	if a > b {
+		return a
+	}
+	return b
+}
+
+func sign(a int) int {
+	if a == 0 {
+		return 0
+	} else if a < 0 {
+		return 1
+	}
+
+	return -1
+}
+
+func barycentric(x int, y int, x1 int, y1 int, x2 int, y2 int, x3 int, y3 int) (int, int, int, int) {
+	det := (x1-x3)*(y2-y3) - (x2-x3)*(y1-y3)
+	u := (y2-y3)*(x-x3) + (x3-x2)*(y-y3)
+	v := (y3-y1)*(x-x3) + (x1-x3)*(y-y3)
+	w := det - u - v
+	return u, v, w, det
+}
+
+func isBarycentricInside(u int, v int, w int, det int) bool {
+	dets := sign(det)
+	us := sign(u) * dets
+	vs := sign(v) * dets
+	ws := sign(w) * dets
+
+	if us >= 0 && vs >= 0 && ws >= 0 {
+		return true
+	}
+
+	return false
+}
+
+func triangleBbox(x1 int, y1 int, x2 int, y2 int, x3 int, y3 int) (int, int, int, int) {
+	left := min(x1, min(x2, x3))
+	bottom := min(y1, min(y2, y3))
+	right := max(x1, max(x2, x3))
+	up := max(y1, max(y2, y3))
+
+	return left, bottom, right, up
+}
+
+func makeColor(r uint32, g uint32, b uint32, a uint32) uint32 {
+	return uint32(0xFF000000 | (b&0xFF)<<16 | (g&0xFF)<<8 | (r & 0xFF))
+}
+
+func (canvas *Canvas) FillTriangle(x1 int, y1 int, x2 int, y2 int, x3 int, y3 int, color uint32) {
+	// canvas.Line(x1, y1, x2, y2, 0xFF2020FF);
+	// canvas.Line(x3, y3, x2, y2, 0xFF2020FF);
+	// canvas.Line(x1, y1, x3, y3, 0xFF2020FF);
+
+	left, bottom, right, up := triangleBbox(x1, y1, x2, y2, x3, y3)
+
+	// canvas.Line(left, bottom, left + w, bottom, 0xFF2020FF);
+	// canvas.Line(left, bottom, left, bottom + h, 0xFF2020FF);
+	// canvas.Line(left + w, bottom, left + w, bottom + h, 0xFF2020FF);
+	// canvas.Line(left, bottom + h, left + w, bottom + h, 0xFF2020FF);
+
+	for y := bottom; y <= up; y++ {
+		for x := left; x <= right; x++ {
+			if isBarycentricInside(barycentric(x, y, x1, y1, x2, y2, x3, y3)) {
+				canvas.PutPixel(x, y, color)
+			}
+		}
+	}
+}
+
+func (canvas *Canvas) FillRGBTriangle(x1 int, y1 int, x2 int, y2 int, x3 int, y3 int) {
+	left, bottom, right, up := triangleBbox(x1, y1, x2, y2, x3, y3)
+
+	for y := bottom; y <= up; y++ {
+		for x := left; x <= right; x++ {
+			u, v, w, det := barycentric(x, y, x1, y1, x2, y2, x3, y3)
+			if isBarycentricInside(u, v, w, det) {
+				canvas.PutPixel(x, y, makeColor(
+					uint32(255*u/det),
+					uint32(255*v/det),
+					uint32(255*w/det),
+					0xFF,
+				))
+			}
+		}
+	}
+}
+
+func NewCanvas(w int, h int) *Canvas {
+	return &Canvas{
+		Width:  w,
+		Height: h,
+		Pixels: make([]uint32, w*h),
+	}
+}
